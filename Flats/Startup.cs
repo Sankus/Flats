816abc20280240ed617ffc38c85df4f8caea6845
Alexey Metelkov <sansan78@mail.ru>
2015-11-20 23:59:23 +0300

    Начальная версия

---
+++ Flats/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Flats.Startup))]
+namespace Flats
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
